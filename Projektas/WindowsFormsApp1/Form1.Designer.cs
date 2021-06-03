4c6c7ac2a25ec47625f4ed2f4a86b6fe52bba3b4
DESKTOP-P3P8N6A\lliut <bacevicius.liutauras@gmail.com>
2021-06-03 17:50:57 +0300

    pradine forma nepilna

--- Projektas/WindowsFormsApp1/Form1.Designer.cs
+++ Projektas/WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,57 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPajamos = new System.Windows.Forms.Button();
+            this.btnDemoPajamos = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPajamos
+            // 
+            this.btnPajamos.Location = new System.Drawing.Point(0, 0);
+            this.btnPajamos.Name = "btnPajamos";
+            this.btnPajamos.Size = new System.Drawing.Size(200, 23);
+            this.btnPajamos.TabIndex = 0;
+            this.btnPajamos.Text = "Pažiūrėti darbuotojo pajamas";
+            this.btnPajamos.UseVisualStyleBackColor = true;
+            // 
+            // btnDemoPajamos
+            // 
+            this.btnDemoPajamos.Location = new System.Drawing.Point(0, 29);
+            this.btnDemoPajamos.Name = "btnDemoPajamos";
+            this.btnDemoPajamos.Size = new System.Drawing.Size(200, 23);
+            this.btnDemoPajamos.TabIndex = 1;
+            this.btnDemoPajamos.Text = "Pajamos pagal pareigas";
+            this.btnDemoPajamos.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(201, 83);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnDemoPajamos);
+            this.Controls.Add(this.btnPajamos);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPajamos;
+        private System.Windows.Forms.Button btnDemoPajamos;
+        private System.Windows.Forms.Button button3;
     }
 }
 
