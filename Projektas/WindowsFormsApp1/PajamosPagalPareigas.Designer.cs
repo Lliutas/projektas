e022c61ab50c0f39df0dfb01b5f1eec617f2a62c
DESKTOP-P3P8N6A\lliut <bacevicius.liutauras@gmail.com>
2021-06-10 17:50:17 +0300

    pabaigiau lenteles, visos pilnai veikia

--- Projektas/WindowsFormsApp1/PajamosPagalPareigas.Designer.cs
+++ Projektas/WindowsFormsApp1/PajamosPagalPareigas.Designer.cs
@@ -29,19 +29,104 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnParodytiGalimasPajamas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtĮvestisEtatoDalis = new System.Windows.Forms.TextBox();
+            this.txtApskaiciuotosPajamos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbxPareigosId = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // btnParodytiGalimasPajamas
+            // 
+            this.btnParodytiGalimasPajamas.Location = new System.Drawing.Point(68, 56);
+            this.btnParodytiGalimasPajamas.Name = "btnParodytiGalimasPajamas";
+            this.btnParodytiGalimasPajamas.Size = new System.Drawing.Size(117, 23);
+            this.btnParodytiGalimasPajamas.TabIndex = 1;
+            this.btnParodytiGalimasPajamas.Text = "Parodyti pajamas";
+            this.btnParodytiGalimasPajamas.UseVisualStyleBackColor = true;
+            this.btnParodytiGalimasPajamas.Click += new System.EventHandler(this.btnParodytiGalimasPajamas_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pareigos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Etato dalis";
+            // 
+            // txtĮvestisEtatoDalis
+            // 
+            this.txtĮvestisEtatoDalis.Location = new System.Drawing.Point(68, 30);
+            this.txtĮvestisEtatoDalis.Name = "txtĮvestisEtatoDalis";
+            this.txtĮvestisEtatoDalis.Size = new System.Drawing.Size(117, 20);
+            this.txtĮvestisEtatoDalis.TabIndex = 4;
+            // 
+            // txtApskaiciuotosPajamos
+            // 
+            this.txtApskaiciuotosPajamos.Location = new System.Drawing.Point(68, 85);
+            this.txtApskaiciuotosPajamos.Name = "txtApskaiciuotosPajamos";
+            this.txtApskaiciuotosPajamos.Size = new System.Drawing.Size(117, 20);
+            this.txtApskaiciuotosPajamos.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pajamos";
+            // 
+            // cmbxPareigosId
+            // 
+            this.cmbxPareigosId.FormattingEnabled = true;
+            this.cmbxPareigosId.Location = new System.Drawing.Point(69, 3);
+            this.cmbxPareigosId.Name = "cmbxPareigosId";
+            this.cmbxPareigosId.Size = new System.Drawing.Size(116, 21);
+            this.cmbxPareigosId.TabIndex = 7;
             // 
             // PajamosPagalPareigas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(197, 107);
+            this.Controls.Add(this.cmbxPareigosId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApskaiciuotosPajamos);
+            this.Controls.Add(this.txtĮvestisEtatoDalis);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnParodytiGalimasPajamas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "PajamosPagalPareigas";
             this.Text = "Pajamos pagal pareigas";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button btnParodytiGalimasPajamas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtĮvestisEtatoDalis;
+        private System.Windows.Forms.TextBox txtApskaiciuotosPajamos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbxPareigosId;
     }
 }
--- Projektas/WindowsFormsApp1/PajamosPagalPareigas.cs
+++ Projektas/WindowsFormsApp1/PajamosPagalPareigas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using MySql.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,74 @@ namespace WindowsFormsApp1
 {
     public partial class PajamosPagalPareigas : Form
     {
+        public void PajamosIdGavimas()
+        {
+            string connectionString = "server=localhost;user=root;database=pajamos;port=3305;password=root";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd;
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "select * from pareigos";
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (cmbxPareigosId != null)
+                {
+                    cmbxPareigosId.Items.Clear();
+                    while (reader.Read())
+                    {
+                        cmbxPareigosId.Items.Add(reader["Pavadinimas"]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            connection.Close();
+        }
+
         public PajamosPagalPareigas()
         {
             InitializeComponent();
+            PajamosIdGavimas();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnParodytiGalimasPajamas_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;database=pajamos;port=3305;password=root";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd;
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "select * from pareigos where Id = " + (cmbxPareigosId.SelectedIndex+1) + ";";
+            int pajamos=0;
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    pajamos = Int32.Parse(reader.GetString(3));
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+            connection.Close();
+            try 
+            { 
+                txtApskaiciuotosPajamos.Text = Convert.ToString(pajamos * Convert.ToDouble(txtĮvestisEtatoDalis.Text)); 
+            }
+            catch (Exception i)
+            { 
+                MessageBox.Show(i.Message); 
+            }
         }
     }
 }
